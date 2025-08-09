cb2d70641f435d837bb79a18974d78e3ebb39128
SURAJ045 <surajmp045@gmail.com>
2025-08-09 20:27:58 +0530

    Updated Features.jsx

--- frontend/portfolio-ui/src/components/Features.jsx
+++ frontend/portfolio-ui/src/components/Features.jsx
@@ -6,9 +6,9 @@ import '../assets/style/Features.css'
 
 const Features = () => {
   const data = [
-    { img: targetIcon, title: "Pixel Perfect", text: "Most common methods for designing websites that work well on desktop is responsive and adaptive design." },
-    { img: paintIcon, title: "High Quality", text: "Most common methods for designing websites that work well on desktop is responsive and adaptive design." },
-    { img: ideaIcon, title: "Awesome Idea", text: "Most common methods for designing websites that work well on desktop is responsive and adaptive design." },
+    { img: targetIcon, title: "Pixel Perfect", text: "Passionate about building visually flawless, blends clean design with robust functionality to deliver seamless user experiences across all devices." },
+    { img: paintIcon, title: "High Quality", text: "Dedicated to delivering visually stunning web applications. Combines precision in design with powerful, reliable functionality for seamless." },
+    { img: ideaIcon, title: "Awesome Idea", text: "High-quality transforms awesome ideas into seamless, user-focused digital experiences with precision and creativity." },
   ];
 
   return (
